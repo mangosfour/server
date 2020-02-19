f880e028c0eb30a0dfc12fbd112073e0cf18a9af
zamalaev <zamalaev88@mail.ru>
2020-02-19 13:44:36 +0000

    CMSG_LIST_INVENTORY 5.4.7 (18019)

--- src/game/Server/Opcodes.h
+++ src/game/Server/Opcodes.h
@@ -483,7 +483,7 @@ enum Opcodes
     CMSG_QUEST_CONFIRM_ACCEPT                             = 0x0D15, // 4.3.4 15595
     SMSG_QUEST_CONFIRM_ACCEPT                             = 0x6F07, // 4.3.4 15595
     CMSG_PUSHQUESTTOPARTY                                 = 0x4B14, // 4.3.4 15595
-    CMSG_LIST_INVENTORY                                   = 0x0346, // 5.4.1 17538
+    CMSG_LIST_INVENTORY                                   = 0x10DD, // 5.4.7 18019
     SMSG_LIST_INVENTORY                                   = 0x7CB0, // 4.3.4 15595
     CMSG_SELL_ITEM                                        = 0x0B7D, // 5.4.1 17538
     SMSG_SELL_ITEM                                        = 0x6105, // 4.3.4 15595
